a3845d34911f416b132295be9881a116da7cb4bb
alex <aleks15.12@mail.ru>
2024-09-25 13:38:37 +0300

    My decision task2

---
+++ Sem1_task1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите натуральное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = -num;
+
+while (i <= num)
+{
+    Console.Write(i);
+    Console.Write(" ");
+    i++;
+}
