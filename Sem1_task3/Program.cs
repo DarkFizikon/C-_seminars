e0d484c2508bb28fa8d1abe8607cf2234e42cb39
alex <aleks15.12@mail.ru>
2024-09-25 14:29:04 +0300

    Sem1 task3 decision

---
+++ Sem1_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// using Internal;
+
+Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num/100 > 0) && (num/100 <= 9))
+{
+    int a = num/100;
+    int b = num%10;
+    int sum = a + b;
+    Console.WriteLine("Сумма чисел = " + sum);
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
