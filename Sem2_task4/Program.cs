3dfc3bdf96a1e7e2d108b7127cd7bbbeac1fcbf0
alex <aleks15.12@mail.ru>
2024-09-26 16:21:53 +0300

    Sem2 task4 my decision

---
+++ Sem2_task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// using Internal;
+
+Console.Write("Введите целое положительное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num >= 100 || num <= -100)
+{
+    while (num > 999 || num < -999)
+    {
+        num = num / 10;
+    }
+    int thirdnum = num % 10;
+    if (thirdnum > 0)
+    {
+        Console.WriteLine($"Третья цифра чиса = {thirdnum}");
+    }
+    else
+    {
+        thirdnum = -thirdnum;
+        Console.WriteLine($"Третья цифра чиса = {thirdnum}");
+    }
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет!");
+}
