c61edf4ccbbf8b9ad8fc2af4da90610bd526f631
alex <aleks15.12@mail.ru>
2024-09-26 14:14:25 +0300

    Sem2 task1 my decision

---
+++ Sem2_task1/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num/100 > 0) && (num/100 <= 9))
+{
+    int a = num%10;
+    int b = num/100;
+    int num2 = b*10 + a;
+    Console.WriteLine($"Новое число = {num2}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
