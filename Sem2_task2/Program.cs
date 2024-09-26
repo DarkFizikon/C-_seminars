c4fea771a8b998bf4d2687297d20ac2f8e534e72
alex <aleks15.12@mail.ru>
2024-09-26 14:37:22 +0300

    Sem2 task2 my decision

---
+++ Sem2_task2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if ((num / 100 > 0) && (num / 100 <= 9))
+{
+    int a = (num / 10) % 10;
+    int b = num % 10;
+    int i = 1;
+    int result = 1;
+    while (i <= b)
+    {
+        result = result * a;
+        i++;
+    }
+    Console.WriteLine($"Результат = {result}");
+}
+else
+{
+    Console.WriteLine("Ошибка ввода!");
+}
