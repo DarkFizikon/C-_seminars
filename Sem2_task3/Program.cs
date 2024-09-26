2b5377074af1f6035805c9991d6fee7d067a8f9a
alex <aleks15.12@mail.ru>
2024-09-26 14:50:46 +0300

    Sem2 task3 my decision

---
+++ Sem2_task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// using Internal;
+
+Console.Write("Введите первое число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int ostatok = num1 % num2;
+if (ostatok == 0)
+{
+    Console.WriteLine("Первое число кратно второму");
+}
+else
+{
+        Console.WriteLine($"Первое число не кратно второму, остаток = {ostatok}");
+}
