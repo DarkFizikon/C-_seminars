f9ada8e43aafaef6446832c1970621d014ec999d
alex <aleks15.12@mail.ru>
2024-10-01 12:51:40 +0300

    Sem3 task3 my decysion

---
+++ Sem3_task3/Program.cs
@@ -0,0 +1,27 @@
+﻿
+int[] array = new int[6];
+
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(-10, 11);
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+if (array.Length % 2 == 0)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int sum = array[i] * array[array.Length - i - 1];
+        Console.Write($"{sum} ");
+    }
+}
+else
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+        int sum = array[i] * array[array.Length - i - 1];
+        Console.Write($"{sum} ");
+    }
+    Console.WriteLine($"(Элемент {array[array.Length / 2 ]} не имеет пары)");
+}
