f26a591042b4e6a31830418a960d8d045d14698f
alex <aleks15.12@mail.ru>
2024-10-01 13:13:06 +0300

    Universal decision task4 seminar3

---
+++ Sem3_task4/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Input number:");
+int num = Convert.ToInt32(Console.ReadLine());
+int numCopy = num;
+int count = 0;
+
+while (numCopy!=0)
+{
+    numCopy/=10;
+    count++;
+}
+
+int [] array = new int [count];
+int index = array.Length - 1;
+while (num !=0)
+{
+    array[index] = num%10;
+    num /=10; //num = num / 10;
+    index--;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
