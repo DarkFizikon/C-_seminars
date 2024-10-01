6e1fd340754bba49fa97aa4a8656216bf78f1b74
alex <aleks15.12@mail.ru>
2024-10-01 12:17:16 +0300

    Sem3 task2 my decysion

---
+++ Sem3_task2/Program.cs
@@ -0,0 +1,15 @@
+﻿int[] array = new int [10];
+
+    Random rnd = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+            array[i] = rnd.Next(-10, 11); 
+            Console.Write($"{array[i]} ");
+    }
+   Console.WriteLine();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = -1 * array[i];
+        Console.Write($"{array[i]} ");
+    }
