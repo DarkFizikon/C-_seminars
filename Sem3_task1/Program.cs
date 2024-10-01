26636e6cbc4233eb23d01814bf54882b9e2a81ec
alex <aleks15.12@mail.ru>
2024-10-01 11:55:23 +0300

    First task with array - defenition is exist num in array

---
+++ Sem3_task1/Program.cs
@@ -0,0 +1,24 @@
+﻿int[] array = { 1, 3, 4, 19, 3 };
+Console.WriteLine("Введите число для проверки: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+bool isExistNum = false;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] == num)
+    {
+        isExistNum = true;
+        break;
+    }
+}
+
+if (isExistNum == true)
+{
+    Console.WriteLine("Yes");
+}
+else
+{ Console.WriteLine("No"); }
+
+string result = isExistNum ? "Yes" : "No"; //Исполнение кода с помощью тернарного оператора можно уопростить до Console.WriteLine(isExistNum ? "Yes" : "No") - будет вывод в одну строку!
+Console.WriteLine(result);
