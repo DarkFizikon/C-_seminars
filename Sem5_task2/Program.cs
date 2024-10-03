ca41506e703fafa02e63466ca74ab743e84889e4
alex <aleks15.12@mail.ru>
2024-10-03 14:20:13 +0300

    Desicion for task2 seminar5

---
+++ Sem5_task2/Program.cs
@@ -0,0 +1,54 @@
+﻿
+int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            matrix[i, j] = rnd.Next(min, max);
+    }
+    return matrix;
+}
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine();
+    }
+}
+
+int SumMainDiag(int[,] matrix)
+{ int sum = 0;
+    for (int i = 0; i < matrix.GetLength(0); i ++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j ++)
+        {
+            if (i == j)
+            {
+                sum = sum + matrix[i,j];
+            }
+        }
+    }
+    return sum;
+}
+
+Console.Write("Input array rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input array columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input min rnd num: ");
+int min = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input max rnd num: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+int[,] mat = CreateMatrixRndInt(rows, columns, min, max);
+ShowMatrix(mat);
+Console.WriteLine();
+int result = SumMainDiag(mat);
+Console.WriteLine($"=> {result}");
