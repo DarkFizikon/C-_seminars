16c5dcb78f816aeeb0d7e7ee914670b51eaff41f
alex <aleks15.12@mail.ru>
2024-10-09 15:51:14 +0300

    Seminar6 task2 change string to array

---
+++ Sem6_task2/Program.cs
@@ -0,0 +1,28 @@
+﻿char[] StringToCharArray(string s)
+{
+    char[] arr = new char[s.Length];
+
+    for (int i = 0; i < s.Length; i++)
+    {
+        arr[i] = s[i];
+    }
+    return arr;
+}
+
+void PrintArray(char[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+            Console.Write($"{array[i]}, ");
+        else
+            Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+Console.Write("Input string: ");
+string str = Console.ReadLine();
+char[] result = StringToCharArray(str);
+PrintArray(result);
