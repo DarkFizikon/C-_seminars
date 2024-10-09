4125fecb484dba77242088f15586f0e1cede4ca6
alex <aleks15.12@mail.ru>
2024-10-09 16:17:55 +0300

    Seminar6 task3 how much vowels in a word

---
+++ Sem6_task3/Program.cs
@@ -0,0 +1,21 @@
+﻿int CountVowels(string str)
+{
+    string vowels = "aeiuyo";
+    int count = 0;
+    for (int i = 0; i < str.Length; i++)
+    {
+        for (int j = 0; j < vowels.Length; j++)
+        {
+            if (vowels[j] == str[i])
+            {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+
+Console.Write("Input string: ");
+string str = Console.ReadLine();
+Console.WriteLine($"In word {CountVowels(str)} vowel's");
