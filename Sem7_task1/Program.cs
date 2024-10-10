ad6cb23f2983c733920cb2faf0cb03d9cca31520
alex <aleks15.12@mail.ru>
2024-10-10 16:55:37 +0300

    Classic recursive function seminars7 task1

---
+++ Sem7_task1/Program.cs
@@ -0,0 +1,11 @@
+﻿void OutNumbers(int n)
+{
+    if (n == 0) return;
+    OutNumbers(n - 1);
+    Console.Write($"{n} ");
+}
+
+Console.Write("Entry natural num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+OutNumbers(num);
