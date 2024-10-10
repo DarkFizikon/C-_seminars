4b889a87c244cc955dcd0a2a646f3faccd0d1276
alex <aleks15.12@mail.ru>
2024-10-10 17:54:30 +0300

    Recursive function semiar7 task2

---
+++ Sem7_task2/Program.cs
@@ -0,0 +1,8 @@
+﻿int SumDigits(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + SumDigits(n / 10);
+}
+Console.Write("Entry natural num: ");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(SumDigits(num)); // 120
