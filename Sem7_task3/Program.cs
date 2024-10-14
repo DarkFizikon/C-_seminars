4c47caca216f5653be089e6e6446d3c99f2d7f1c
alex <aleks15.12@mail.ru>
2024-10-14 16:45:15 +0300

    Vowaels and consanants check and print

---
+++ Sem7_task3/Program.cs
@@ -0,0 +1,22 @@
+﻿void PrintConsanants(string text, int i = 0)
+{
+    if (i == text.Length)
+    {
+        return;
+    }
+    CheckConsanats(text, i);
+    PrintConsanants(text, i + 1);
+}
+
+void CheckConsanats(string text, int i)
+{
+    string vowels = "aeiouy";
+    if (char.IsAsciiLetter(text[i]) && !vowels.Contains(text[i]))
+    {
+        Console.Write($"{text[i]}");
+    }
+}
+
+Console.Write("Entry string: ");
+string str = Console.ReadLine();
+PrintConsanants(str);
